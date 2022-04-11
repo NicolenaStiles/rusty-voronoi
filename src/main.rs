8261a35322e6e0dfb3ca63b82ead06d958a7905b
NicolenaStiles <nicolena.stiles@gmail.com>
2022-04-11 06:00:14 -0400

    think I got the logic for the voronoi calcs.

--- src/main.rs
+++ src/main.rs
@@ -19,14 +19,21 @@ use bmp::{Image, Pixel};
 // random seed/site generation
 use rand::Rng;
 
+// graph/the entire plot
 struct VoronoiPlot {
     grid_squares: Vec<Vec<VoronoiUnit>>,
 }
 
+// subunit of the plot. basically a pixel.
+// defaults:
+// is_site = false
+// closest_site = (0,0)
+// proximity = 0.0
 #[derive(Clone)]
 struct VoronoiUnit {
     is_site: bool,
-    proximity: float,
+    closest_site: Vec<u32>,
+    proximity: f64,
 }
 
 // generate some random points for a "good" Voronoi diagram
@@ -57,10 +64,65 @@ fn main() {
     let img_pad: u32 = 16;
 
     // quick test for site generation
-    let quicktest: Vec<Vec<u32>> = generate_rand_pts(num_pts, img_res, img_res, img_pad, img_pad);
+    let mut site_array: Vec<Vec<u32>> =
+        generate_rand_pts(num_pts, img_res, img_res, img_pad, img_pad);
 
-    println!("{:?}", quicktest);
+    // spinning up empty voronoi graph
+    let mut grid_init = Vec::new();
+    for x in 0..img_res {
+        let mut row_init = Vec::new();
+        for y in 0..img_res {
+            let mut single_graph_obj = VoronoiUnit {
+                is_site: false,
+                closest_site: vec![0, 0],
+                proximity: 0.0,
+            };
+            row_init.push(single_graph_obj);
+        }
+        grid_init.push(row_init);
+    }
 
+    // load voronoi graph with sites
+    // (flippin' some bools and making nearest site self)
+    for site in &site_array {
+        let x_site = site[0 as usize];
+        let y_site = site[1 as usize];
+        grid_init[x_site as usize][y_site as usize].is_site = true;
+        grid_init[x_site as usize][y_site as usize].closest_site[0 as usize] = x_site;
+        grid_init[x_site as usize][y_site as usize].closest_site[1 as usize] = y_site;
+    }
+
+    // actual voronoi pts calculations!
+    // haven't tested this at all, but I think it works.
+    // TODO: fix the freaking type handling. Sheesh.
+    for x in 0..img_res {
+        for y in 0..img_res {
+            // for each point...
+            let mut curr_pt = &grid_init[x as usize][y as usize];
+            // if not a site...
+            if curr_pt.is_site == false {
+                // calculate distance to all sites...
+                let mut min_dist: f64 = img_res as f64;
+                for site in &site_array {
+                    let mut x_diff =
+                        f64::powf(((site[0 as usize] as f64) - (x as f64)) as f64, 2.0);
+                    let mut y_diff =
+                        f64::powf(((site[1 as usize] as f64) - (y as f64)) as f64, 2.0);
+                    let mut curr_dist = f64::powf((x_diff + y_diff) as f64, 0.5);
+                    if curr_dist < min_dist {
+                        min_dist = curr_dist;
+                        grid_init[x as usize][y as usize].closest_site[0 as usize] =
+                            site[0 as usize];
+                        grid_init[x as usize][y as usize].closest_site[1 as usize] =
+                            site[1 as usize];
+                        grid_init[x as usize][y as usize].proximity = min_dist;
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{:?}", site_array);
     // resolution of image to create (pixels map directly)
     /*
     let mut img = Image::new(512, 512);
